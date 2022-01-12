c4a99d2d30da2e372b3510cdd72f35282833243b
Anthony Linebaugh <anthonylinebaugh95@gmail.com>
2022-01-11 21:06:28 -0500

    added a type Profile in typeDefs.js in schemas

--- server/schemas/typeDefs.js
+++ server/schemas/typeDefs.js
@@ -1,6 +1,16 @@
 const { gql } = require("apollo-server-express");
 
 const typeDefs = gql`
+    type Profile {
+        _id: ID
+        name: String
+        email: String
+        password: String
+        role: String
+        inventory: [String]!
+        gold: Int
+    }
+
     type Query{
     }
 
