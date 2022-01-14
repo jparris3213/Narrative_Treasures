9a32e66c4b8de4419a5cafcf9d41c2c1ef956276
Anthony Linebaugh <anthonylinebaugh95@gmail.com>
2022-01-13 20:11:23 -0500

    added types Profile, and Item to typeDefs and added Query and Mutation, adding code to resolvers.js now

--- server/schemas/resolver.js
+++ server/schemas/resolver.js
@@ -1,9 +1,29 @@
 const { AuthenticationError } = require("apollo-server-express");
-const {} = require("../models");
+const { Profile } = require("../models");
+const { Item } = require("../models");
 const { signToken } = require("../utils/auth");
 
 const resolver = {
-  Query: {},
+  Query: {
+    profiles: async () => {
+      return Profile.find();
+    },
+    profile: async (parent, { profileId }) => {
+      return Profile.findOne({ _id: profileId });
+    },
+    me: async (parent, args, context) => {
+      if (context.user) {
+        return Profile.findOne({ _id: context.user._id });
+      }
+      throw new AuthenticationError("You need to be logged in!");
+    },
+    items: async () => {
+      return Item.find();
+    },
+    item: async (parent, { itemId }) => {
+      return Item.findOne({ _id: itemId });
+    },
+  },
 
   Mutation: {},
 };
--- server/schemas/typeDefs.js
+++ server/schemas/typeDefs.js
@@ -1,21 +1,54 @@
 const { gql } = require("apollo-server-express");
 
 const typeDefs = gql`
-    type Profile {
-        _id: ID
-        name: String
-        email: String
-        password: String
-        role: String
-        inventory: [String]!
-        gold: Int
-    }
+  type Profile {
+    _id: ID!
+    name: String!
+    email: String!
+    password: String!
+    role: String
+    inventory: [String]
+    gold: Int!
+  }
 
-    type Query{
-    }
+  type Item {
+    _id: ID!
+    name: String!
+    cost: Int!
+    equipmentType: String!
+    description: String
+  }
 
-    type Mutation {
-    }
+  type Query {
+    profiles: [Profile]!
+    profile(profileId: ID!): Profile
+    me: Profile
+    items: [Item]!
+    item(itemId: ID!): Item
+  }
+
+  type Mutation {
+    addProfile(
+      name: String!
+      email: String!
+      password: string!
+      role: String!
+      gold: Int!
+    ): Auth
+    login(name: String!, email: String!, password: String!): Auth
+
+    changeGold(profileId: ID!, gold: Int!): Profile
+    addInventory(profileId: ID!, inventory: String!): Profile
+    changeRole(profileId: ID!, role: String!): Profile
+
+    addItem(
+      name: String!
+      cost: Int!
+      equipmentType: String!
+      description: String!
+    ): Item
+    removeItem(item: String): Item
+  }
 `;
 
 module.exports = typeDefs;
