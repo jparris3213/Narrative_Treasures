40c1efca7269756b1e961e76e32b78646069f811
Anthony Linebaugh <anthonylinebaugh95@gmail.com>
2022-01-18 19:11:50 -0500

    added the mutations and queries to the client side

--- client/src/utils/mutation.js
+++ client/src/utils/mutation.js
@@ -1,3 +1,46 @@
 import { gql } from "@apollo/client";
 
-// mutations
+//this adds a user to our database
+export const ADD_PROFILE = gql`
+  mutation addProfile(
+    $name: String!
+    $email: String!
+    $password: String!
+    $dungeonMaster: Boolean!
+    $gold: Int!
+  ) {
+    addProfile(
+      name: $name
+      email: $email
+      password: $password
+      dungeonMaster: $dungeonMaster
+      gold: $gold
+    ) {
+      token
+      profile {
+        _id
+        name
+      }
+    }
+  }
+`;
+
+//mutation used for changing users gold count, must first do the math on the current gold count the user has adding/subtracting, get the answer and use that for the mutation
+export const CHANGE_GOLD = gql`
+  mutation changeGold($profileId: ID!, $goldSet: Int!) {
+    changeGold(profileId: $profileId, goldSet: $goldSet) {
+      name
+      gold
+    }
+  }
+`;
+
+//mutation used to change the users role of DM to true or false
+export const CHANGE_ROLE = gql`
+  mutation changeRole($profileId: ID!, $setRole: Boolean!) {
+    changeRole(profileId: $profileId, setRole: $setRole) {
+      name
+      dungeonMaster
+    }
+  }
+`;
