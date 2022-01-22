1605cf718740411120f92ba985915b0a7db5f7c6
Jacob Parris <typefortyproductions@gmail.com>
2022-01-22 10:27:59 -0500

    Combined Inventory with User Profile, removed Navbar Link to Inventory

--- client/src/Pages/UserProfile.js
+++ client/src/Pages/UserProfile.js
@@ -1,6 +1,22 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import Item from "../components/LineItem";
+import {
+  MAGIC_ITEMS,
+  ALL_EQUEPMENT,
+  MONSTERS_QUERY,
+  MONSTER_QUERY,
+} from "../utils/api";
 
 const UserProfile = () => {
+
+  const [equipments, setEquipments] = useState([]);
+  useEffect(() => {
+    ALL_EQUEPMENT().then(({ data }) => {
+      setEquipments(data.equipments);
+    });
+  }, []);
+
+
   return (
     <div>
       <div>
@@ -15,6 +31,35 @@ const UserProfile = () => {
           <li>Current Gold: </li>
           <li>Sold</li>
         </ul>
+      </div>
+      <div>
+        <div className="container-fluid">
+          <div className="row">
+            <h2>Current Inventory</h2>
+            <div className="table-responsive">
+              <table className="table table-striped table-sm">
+                <thead>
+                  <tr>
+                    <th scope="col">Item</th>
+                    <th scope="col">Qty</th>
+                    <th scope="col">Description</th>
+                    <th scope="col">Dice</th>
+                    <th scope="col">Cost</th>
+                    <th scope="col">Market Price</th>
+                    <th scope="col">Profit/Loss</th>
+                  </tr>
+                </thead>
+                <tbody>
+                    {equipments.length &&
+                      equipments.map((equipment) => {
+                        return <Item equipment={equipment} key={equipment.index} />;
+                      })}
+                </tbody>
+              </table>
+            </div>
+            
+          </div>
+        </div>
       </div>
     </div>
   );
